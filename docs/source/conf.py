5ec34b0e819c9ac83c773d5dc543dca3dcea3555
Jacob Hartman <junyunharold1@gmail.com>
2021-03-03 22:29:29 -0500

    Doc fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
+# import os
+# import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.append(os.path.abspath("./_ext"))
 
 import sphinx_rtd_theme
 
@@ -34,8 +33,7 @@ release = '0.6.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
-    "module_parse"
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
