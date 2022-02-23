8fb3234118d2cf8ef320fc55e13adc67a186c706
Jacob Hartman <junyunharold1@gmail.com>
2022-02-22 22:19:28 -0500

    More trying

--- scripts/github/run_github_tests.sh
+++ scripts/github/run_github_tests.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+COLOR_RED="\e[1;31m"
+COLOR_BLUE="\e[1;34m"
+COLOR_GREEN="\e[1;32m"
+COLOR_ORANGE="\e[1;33m"
+COLOR_RESET="\e[0m"
+
+echo "${COLOR_GREEN}Starting Tests!${COLOR_RESET}"
 cd /opt/fakernet 
 source ./venv/bin/activate 
-python3 build.py 
+
+echo "${COLOR_BLUE}Cleaning...${COLOR_RESET}"
 python3 tools/full-clean.py -f
 python3 tools/clean-images.py -f
+echo "${COLOR_BLUE}Building...${COLOR_RESET}"
 python3 build.py
+echo "${COLOR_BLUE}Testing...${COLOR_RESET}"
 python3 test/test_all.py -f
